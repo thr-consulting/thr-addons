6b5c3794417fe0e0dbcd8e6432ed3fe4a3a2e091
darkadept <darkadept@durbn.net>
2018-06-19 14:42:19 -0500

    * Fixed GraphQL Money scalar to accept object, string and number values. * Enhanced TForm handleChange() to be passed a field name to accommodate controls that don't use SyntheticEvent.

--- packages/money/src/graphql/GraphQLMoney.js
+++ packages/money/src/graphql/GraphQLMoney.js
@@ -1,7 +1,9 @@
 import {GraphQLError} from 'graphql/error';
-import {GraphQLScalarType} from 'graphql';
+import {GraphQLScalarType} from 'graphql/type';
 import {Kind} from 'graphql/language';
 import isString from 'lodash/isString';
+import isObject from 'lodash/isObject';
+import isNumber from 'lodash/isNumber';
 import Money from 'js-money';
 import {makeMoney} from '../util';
 
@@ -10,12 +12,14 @@ const GraphQLMoney = new GraphQLScalarType({
 	description: 'JS-Money object',
 	// Parses variable values (JSON => JSON)
 	parseValue(value) {
-		if (isString(value)) return makeMoney(Number(value));
 		return makeMoney(value);
 	},
 	// Called when the value is being sent to the client
 	serialize(value) {
 		if (value instanceof Money) return value;
+		if (isObject(value) && value.amount && value.currency) return makeMoney(value);
+		if (isString(value)) return makeMoney(value);
+		if (isNumber(value)) return makeMoney(value);
 		throw new GraphQLError('Trying to serialize a non-primitive');
 	},
 	// Parses GraphQL language AST into the value. (AST => JSON)
