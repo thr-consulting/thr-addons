627982d5fcc7d5e957c6e06bd2633383592fba5e
darkadept <darkadept@durbn.net>
2019-03-20 18:10:31 -0500

    removing some packages moving to webpack cleaning things up

---
+++ packages/money/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+	mode: process.env.NODE_ENV,
+	devtool: 'source-map',
+	entry: './src/index.js',
+	output: {
+		filename: 'money.js',
+		path: path.resolve(__dirname, 'lib'),
+		library: 'money',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+	},
+	externals: [
+		nodeExternals({modulesDir: path.join('..', '..', 'node_modules')}),
+	],
+	optimization: {
+		minimize: process.env.NODE_ENV === 'production',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: '/node_modules/',
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							babelrc: false,
+							presets: [['@imperium/babel-preset-imperium']],
+						},
+					},
+				],
+			},
+		],
+	},
+};
