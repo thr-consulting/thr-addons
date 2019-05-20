f87da770a3ba08bf0df3454648c6946137fb7e1c
Mike Kornelson <darkadept@durbn.net>
2019-05-19 22:57:06 -0500

    Feature/extras (#87)
    * added a @thx/extras package
    
    * Disabled tests for extra for now
    * Fixed some SharedCache code

---
+++ packages/extras/client.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const client = require('./lib/client');
+
+module.exports = client.default;
---
+++ packages/extras/server.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const server = require('./lib/server');
+
+module.exports = server.default;
