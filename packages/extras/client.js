644c8a0e92ca8c9e39910aaf6edf946e1895afa4
darkadept <darkadept@durbn.net>
2019-05-20 08:03:41 -0500

    fixing deploy

--- packages/extras/client.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const client = require('./lib/client');
-
-module.exports = client.default;
--- packages/extras/server.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const server = require('./lib/server');
-
-module.exports = server.default;
