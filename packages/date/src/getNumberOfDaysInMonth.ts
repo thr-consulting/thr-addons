8af659696301123f1d91270f127ad7b958742191
darkadept <darkadept@durbn.net>
2019-05-19 23:02:31 -0500

    fixed a missed function name

--- packages/date/src/getNumberOfDaysInMonth.ts
+++ packages/date/src/getNumberOfDaysInMonth.ts
@@ -5,12 +5,12 @@ interface GetMonthDaysParams {
 }
 
 /**
- * Returns the number of days in for a given month.
+ * Returns the number of days in a given month.
  * @param month: {number}, The number of the month to get where January = 1.
  * @param limitToCurrentDay = false: {boolean}, If set to true, returns number of days up to current day.
  * @param asArray = false: {boolean}, If set to true, returns array of days.
  */
-export default function getMonthDays({month, limitToCurrentDay = false, asArray = false}: GetMonthDaysParams) {
+export default function getNumberofDaysInMonth({month, limitToCurrentDay = false, asArray = false}: GetMonthDaysParams) {
 	const days = (limitToCurrentDay && (month === new Date().getMonth() + 1))
 		? new Date().getDate()
 		: new Date(2019, month, 0).getDate();
