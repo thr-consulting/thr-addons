7680e9fc74483e8a8e8dac2033a6db2fe7bff475
darkadept <darkadept@durbn.net>
2019-05-20 08:57:46 -0500

    forgot to export SharedCache

--- packages/extras/src/server/index.ts
+++ packages/extras/src/server/index.ts
@@ -4,3 +4,4 @@ export {default as sha256} from '../common/sha256';
 export {default as CounterStream} from './CounterStream';
 export {default as log, configureLog, waitForLogger, logError} from './log';
 export {readableSpy, writableSpy} from './streamSpy';
+export {default as SharedCache} from './SharedCache';
