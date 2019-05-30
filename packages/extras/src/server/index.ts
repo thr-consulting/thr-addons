eb4eb028e5df9b435edd34b9f740250c298b3c31
darkadept <darkadept@durbn.net>
2019-05-30 11:29:21 -0500

    * Moved to node 8.16.0 * Fixed a bug in `transformDataToLocalDateTime`. * Added a `randomFilename` method to @thx/extras.

--- packages/extras/src/server/index.ts
+++ packages/extras/src/server/index.ts
@@ -1,4 +1,4 @@
-export {randomAlphanumeric, randomAlphanumericCharacter, randomId, randomLetterCharacter, randomLetters} from '../common/random';
+export {randomAlphanumeric, randomAlphanumericCharacter, randomId, randomLetterCharacter, randomLetters, randomFilename} from '../common/random';
 export {default as createDataLoader} from './createDataLoader';
 export {default as sha256} from '../common/sha256';
 export {default as CounterStream} from './CounterStream';
