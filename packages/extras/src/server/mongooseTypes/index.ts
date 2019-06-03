c8c5f062bb9f7b16019de2de42f53e968c051425
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2019-06-03 09:31:00 -0500

    Refactoring and improvements

--- packages/extras/src/server/mongooseTypes/index.ts
+++ packages/extras/src/server/mongooseTypes/index.ts
@@ -9,26 +9,31 @@ import isNull from 'lodash/isNull';
 
 const d = debug('app.lib.mongooseTypes');
 
-// Add .toBSON to LocalDate if not defined
-// @ts-ignore
-if (!LD.prototype.toBSON) {
+function toBSON(): number {
 	// @ts-ignore
-	LD.prototype.toBSON = function() {
-		d('toBSON', this);
-		return transformLocalDateToEpochInteger(this);
-	};
+	d('toBSON', this);
+	// @ts-ignore
+	return transformLocalDateToEpochInteger(this);
 }
 
+// Add .toBSON to LocalDate if not defined
+// @ts-ignore
+if (!LD.prototype.toBSON) { LD.prototype.toBSON = toBSON; }
+
 class LocalDate extends mongoose.SchemaType {
+	$conditionalHandlers: object;
+
 	constructor(...params) {
+		// @ts-ignore
 		super(...params);
 
 		this.$conditionalHandlers = {
+		// @ts-ignore
 			...mongoose.SchemaType.prototype.$conditionalHandlers,
-			$gt: this.handleSingle,
-			$gte: this.handleSingle,
-			$lt: this.handleSingle,
-			$lte: this.handleSingle,
+			$gt: this.cast,
+			$gte: this.cast,
+			$lt: this.cast,
+			$lte: this.cast,
 		};
 	}
 
@@ -41,13 +46,25 @@ class LocalDate extends mongoose.SchemaType {
 
 		throw new Error(`SchemaType LocalDate: ${val} is not a LocalDate (or null, undefined)`);
 	}
-
-	handleSingle(val) {
-		return this.cast(val);
-	}
 }
 
 class Money extends mongoose.SchemaType {
+	$conditionalHandlers: object;
+
+	constructor(...params) {
+		// @ts-ignore
+		super(...params);
+
+		this.$conditionalHandlers = {
+			// @ts-ignore
+			...mongoose.SchemaType.prototype.$conditionalHandlers,
+			$gt: this.cast,
+			$gte: this.cast,
+			$lt: this.cast,
+			$lte: this.cast,
+		};
+	}
+
 	cast(val: any) {
 		d('Cast:', val);
 		if (isNull(val)) return null;
