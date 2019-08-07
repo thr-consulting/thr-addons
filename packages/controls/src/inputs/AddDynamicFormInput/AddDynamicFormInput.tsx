c492394ecd9c269391c54491a756140e0134fb39
Jonathan Spomer <38764827+JohnnySpomer@users.noreply.github.com>
2019-08-07 11:02:43 -0500

    Updated the AddDynamicFormInput component.  (#119)
    * Updated the AddDynamicFormInput component. It can now handle maskedInputs as a child of FormInput.
    
    * Updated the test.
    closes darkadept/thr4#297

--- packages/controls/src/inputs/AddDynamicFormInput/AddDynamicFormInput.tsx
+++ packages/controls/src/inputs/AddDynamicFormInput/AddDynamicFormInput.tsx
@@ -12,10 +12,10 @@ interface Props {
 	setFieldValue: (position: string, value: {}) => void,
 	name: string,
 	label: string,
-	disableAddName?: boolean,
+	disableAddButton?: boolean,
 	children: ReactElement[],
 	fieldError: (name: string | string[]) => boolean,
-	onBlur: (e: any) => void,
+	handleBlur: (e: any) => void,
 }
 
 /**
@@ -24,68 +24,66 @@ interface Props {
  * @constructor
  */
 export default function AddDynamicFormInput(props: Props) {
-	const handleChange = (e, child): void => {
-		const {name, value, index} = child;
-		props.setFieldValue(`${props.name}[${index}][${name}]`, value);
-	};
-
-	const renderAddButton = (index, label, arrayHelpers) => (
+	const renderAddButton = (label, arrayHelpers) => (
 		<Form.Input style={{marginTop: 25}}>
+			<Button color="green" disabled={props.disableAddButton} onClick={() => arrayHelpers.push({})} type="button">Add</Button>
 			<Label fluid="true" size="large">{label}</Label>
-			<Button color="green" disabled={props.disableAddName} onClick={() => arrayHelpers.push({})} type="button">Add</Button>
 		</Form.Input>
 	);
 
-	const renderGrid = (arrayHelpers, arrayObj = {}, rowIndex = 0, length = 0) => {
-		const {children, label = 'label', fieldError, name, onBlur} = props;
-		if (!children) throw new Error('There are no children defined for AddDynamicFormInput');
+	const renderRemoveButton = (index, arrayHelpers) => (
+		<Form.Input style={{marginTop: 25}}>
+			<Button color="red" onClick={() => arrayHelpers.remove(index)} type="button">Remove</Button>
+		</Form.Input>
+	);
+
+	const renderChild = (Child, rowIndex, arrayHelpers, arrayObj, cName = null) => {
+		const {name, fieldError, handleBlur} = props;
+		const {onChange, value, error, children, name: childName, ...rest} = Child.props; // eslint-disable-line
+		const n = `${name}.${rowIndex.toString()}.${childName || cName}`;
+		const p = (children) ? {
+			name: childName,
+			error: fieldError(n),
+			children: renderChild(children[0] || children, rowIndex, arrayHelpers, arrayObj, childName),
+			...rest,
+		} : {
+			name: n,
+			onBlur: handleBlur,
+			error: fieldError(n),
+			onChange: (arg1, arg2) => props.setFieldValue(n, arg2 ? arg2.value : arg1),
+			value: arrayObj[childName || cName] || '',
+			...rest,
+		};
+
+		return (cloneElement(Child, p));
+	};
+
+	const renderGrid = (arrayHelpers, arrayObj = {}, rowIndex = 0) => {
+		const {children, label = 'label', name} = props;
+		if (!children) throw new Error('There are no children defined for the AddDynamicFormInput component');
 
 		return (
 			<Grid stackable columns={children.length + 1 as SemanticWIDTHS} key={rowIndex}>
 				<Grid.Row>
 					<Grid.Column>
-						{rowIndex < 1 ? renderAddButton(length, label, arrayHelpers) : null}
+						{rowIndex < 1 ? renderAddButton(label, arrayHelpers) : renderRemoveButton(rowIndex, arrayHelpers)}
 					</Grid.Column>
-					{Children.map(children, (Child: ReactElement, columnIndex) => {
-						const {type, props: childProps} = Child;
-						if (type !== FormInput) return <Form.Input label="Invalid Input Provided" placeholder="Must be a Form.Input" error/>;
-						// eslint-disable-next-line
-						const {onChange, value, error, name: childName, ...rest} = childProps;
-
-						// if there is more than one column and it is the last child element.
-						const deleteAction = (
-							(length > 1) && (columnIndex === children.length - 1))
-							? {icon: 'delete', color: 'red', onClick: () => arrayHelpers.remove(rowIndex)}
-							: null;
-
-						const childElement = (
-							cloneElement(Child, {
-								action: deleteAction,
-								onChange: handleChange,
-								index: rowIndex,
-								value: arrayObj[childProps.name] || '',
-								error: fieldError([name, rowIndex.toString(), childName]),
-								onBlur,
-								...rest,
-							}));
-
-						return (<Grid.Column key={name}>{childElement}</Grid.Column>);
+					{Children.map(children, (Child: ReactElement) => {
+						if (Child.type !== FormInput) return <Form.Input label="Invalid Input Provided" placeholder="Must be a Form.Input" error/>;
+						return (<Grid.Column key={name}>{renderChild(Child, rowIndex, arrayHelpers, arrayObj)}</Grid.Column>);
 					})}
 				</Grid.Row>
 			</Grid>
 		);
 	};
 
-	const renderGridWithValues = arrayHelpers => {
-		const {values} = props;
-		return values.map((arrayObj, index) => renderGrid(arrayHelpers, arrayObj, index, values.length));
-	};
-
 	return (
 		<FieldArray
 			name={props.name}
 			render={arrayHelpers => (
-				isEmpty(props.values) ? renderGrid(arrayHelpers) : renderGridWithValues(arrayHelpers)
+				isEmpty(props.values)
+					? renderGrid(arrayHelpers)
+					: props.values.map((arrayObj, index) => renderGrid(arrayHelpers, arrayObj, index))
 			)}
 		/>
 	);
