ed8ecb57cf563c780af77928c069a5a7ea01ec2a
Jonathan <j.spomer@makingyourmilescount.com>
2020-03-12 12:02:36 -0500

    Fixed lint. Closes #184

--- packages/controls/src/inputs/Scriptel/ScriptelContext.ts
+++ packages/controls/src/inputs/Scriptel/ScriptelContext.ts
@@ -2,4 +2,6 @@ import React, {MutableRefObject} from 'react';
 import {RenderedImage} from './scriptel/messages';
 import {ScriptelSocket} from './scriptel';
 
-export const ScriptelContext = React.createContext<{socket: MutableRefObject<ScriptelSocket | undefined>; renderImage: RenderedImage | undefined} | undefined>(undefined);
+export const ScriptelContext = React.createContext<
+	{socket: MutableRefObject<ScriptelSocket | undefined>; renderImage: RenderedImage | undefined} | undefined
+>(undefined);
