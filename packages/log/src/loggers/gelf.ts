e9140326cd0bbae3afaee87b12f89efad16305a3
darkadept <darkadept@durbn.net>
2020-09-28 15:47:43 -0500

    feat(log): fixed up logging code

---
+++ packages/log/src/loggers/gelf.ts
@@ -0,0 +1,83 @@
+import winston, {format} from 'winston';
+import Transport, {TransportStreamOptions} from 'winston-transport';
+import Gelf, {Logger} from 'gelf-pro';
+
+interface GelfTransportOptions extends TransportStreamOptions {
+	gelfPro: any; // This should be gelf-pro.Settings but it's too restrictive.
+}
+
+class GelfTransport extends Transport {
+	private logger: Logger;
+
+	constructor(opts: GelfTransportOptions) {
+		super(opts);
+		this.logger = Object.create(Gelf);
+		this.logger.setConfig(opts.gelfPro);
+	}
+
+	log({level, message, ...meta}: {level: string; message: string; meta: any}, next: () => void): any {
+		setImmediate(() => {
+			this.emit('logged', {level, message, meta});
+		});
+
+		switch (level) {
+			case 'emerg':
+				this.logger.emergency(message, meta);
+				break;
+			case 'alert':
+				this.logger.alert(message, meta);
+				break;
+			case 'crit':
+				this.logger.critical(message, meta);
+				break;
+			case 'error':
+				this.logger.error(message, meta);
+				break;
+			case 'warn':
+				this.logger.warn(message, meta);
+				break;
+			case 'notice':
+				this.logger.notice(message, meta);
+				break;
+			case 'info':
+			default:
+				this.logger.info(message, meta);
+				break;
+			case 'debug':
+				this.logger.debug(message, meta);
+				break;
+		}
+
+		next();
+	}
+
+	setConfig(opts: GelfTransportOptions) {
+		this.logger.setConfig(opts.gelfPro);
+	}
+}
+
+export interface GelfLoggerParams {
+	host: string;
+	proto: 'tcp-tls' | 'tcp' | 'udp';
+	port: number;
+}
+
+export function createGelfLogger({host, proto, port}: GelfLoggerParams) {
+	const gelfFormat = winston.format(info => {
+		// Pull the error stack out properly if it exists
+		if (info.error && info.error.stack) {
+			return {...info, stack: info.error.stack};
+		}
+		return info;
+	});
+	return new GelfTransport({
+		gelfPro: {
+			adapterName: proto,
+			adapterOptions: {
+				host,
+				port,
+			},
+		},
+		format: format.combine(format.timestamp(), gelfFormat()),
+	});
+}
