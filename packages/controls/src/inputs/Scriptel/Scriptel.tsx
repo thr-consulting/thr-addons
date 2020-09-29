df02db579180c2a6483f819bdd896aa53b9b6776
Jonathan Spomer <38764827+JohnnySpomer@users.noreply.github.com>
2020-09-29 09:28:00 -0500

    Scriptel loading indicator fix (#227)
    * fix(controls): fixed scriptel input loading indicator
    
    * fix(controls): fixed scriptel input loading indicator, closes #220

--- packages/controls/src/inputs/Scriptel/Scriptel.tsx
+++ packages/controls/src/inputs/Scriptel/Scriptel.tsx
@@ -32,15 +32,12 @@ export function Scriptel({omniscriptUrl, imageType, scale, crop, penStyle, child
 		});
 
 		socket.current.on('render', msg => {
-			setLoading(false);
 			setRender(msg);
+			setLoading(false);
+			setRender(undefined); // todo look at this in future
 		});
 		socket.current.on('okButtonDown', () => {
 			setLoading(true);
-		});
-		socket.current.on('okButtonPress', () => {
-			setLoading(false);
-			setRender(undefined);
 		});
 		socket.current.on('cancel', () => {
 			setLoading(false);
