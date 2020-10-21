d92111dc4cbe5552284e97568fa3afe4e082ad5c
Jonathan Spomer <38764827+JohnnySpomer@users.noreply.github.com>
2020-10-21 09:42:59 -0500

    Update scriptel schema type (#229)
    * fix(controls): updated scriptelSchemaType
    
    * fix(controls): updated the scriptelSchemaType

--- packages/controls/src/yupTypes/scriptelSchemaType.ts
+++ packages/controls/src/yupTypes/scriptelSchemaType.ts
@@ -3,11 +3,11 @@ import {object, number, string, date} from 'yup';
 export function scriptelSchemaType() {
 	return object()
 		.shape({
-			width: number().notRequired(),
-			height: number().notRequired(),
+			width: number().notRequired().nullable(), // by what I understand making it nullable allows null and notRequired allows for undefined
+			height: number().notRequired().nullable(),
 			data: string().required(),
-			timestamp: date().notRequired(),
-			type: string().oneOf(['image/svg+xml', 'image/png']).notRequired(),
+			timestamp: date().notRequired().nullable(),
+			type: string().oneOf(['image/svg+xml', 'image/png']).notRequired().nullable(),
 		})
 		.required()
 		.default(undefined);
