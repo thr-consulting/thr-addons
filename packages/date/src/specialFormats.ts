29a767bab7e4d59d96c9f9caff2f18d91ad8dd26
darkadept <darkadept@durbn.net>
2020-10-22 13:59:29 -0500

    feat(date): added getTimeOfDay() method

---
+++ packages/date/src/specialFormats.ts
@@ -0,0 +1,16 @@
+import {ChronoField, LocalTime} from '@js-joda/core';
+
+interface TimeOfDay {
+	time: string;
+	timeOfDay: string;
+}
+
+export function getTimeOfDay(time: LocalTime): TimeOfDay {
+	return {
+		time: time
+			.get(ChronoField.CLOCK_HOUR_OF_AMPM)
+			.toString()
+			.concat(':', time.minute() < 10 ? '0' : '', time.minute().toString()),
+		timeOfDay: time.hour() < 12 ? 'morning' : 'afternoon',
+	};
+}
