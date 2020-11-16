c48ec7a02960a2c34773cf318fa1e668c3ffa6e8
darkadept <darkadept@durbn.net>
2020-11-16 13:53:17 -0600

    feat: upgrade to storybook 6

--- packages/controls/.storybook/main.js
+++ packages/controls/.storybook/main.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const webpackConfig = require('../webpack');
 const compact = require('lodash/compact');
 
@@ -8,23 +9,26 @@ module.exports = {
 
 		// This is dependant on storybook's config
 		delete theirs.module.rules[0]; // Remove JS rule
-		delete theirs.module.rules[2]; // Remove CSS rule
+		delete theirs.module.rules[1]; // Remove JS rule
+		delete theirs.module.rules[3]; // Remove CSS rule
 		delete theirs.plugins[6]; // Remove progress plugin
 
 		const ourRules = [...webpackConfig.module.rules];
 		// This is highly dependant on @thx/common-webpack config
-		ourRules[1].use[1].options.presets[0][1].reactRefresh = false
+		ourRules[0].use[1].options = {...ourRules[0].use[1].options, sourceType: 'unambiguous'};
+		ourRules[1].use[1].options = {...ourRules[1].use[1].options, sourceType: 'unambiguous'};
+		ourRules[1].use[1].options.presets[0][1].reactRefresh = false;
 
-		return {
+		const cnf = {
 			...config,
 			resolve: webpackConfig.resolve,
 			plugins: compact(theirs.plugins),
 			module: {
-				rules: [
-					...ourRules,
-					...compact(theirs.module.rules),
-				],
+				rules: [...ourRules, ...compact(theirs.module.rules)],
 			},
 		};
+
+		// console.log(util.inspect(cnf, true, null, true));
+		return cnf;
 	},
 };
