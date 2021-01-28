6830187c5b582b6ebaa23d452dbbb5f489b4104c
darkadept <darkadept@durbn.net>
2021-01-28 10:19:41 -0600

    feat(controls): added DropdownCell for TableInput

---
+++ packages/controls/src/inputs/TableInput/DropdownCell.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Dropdown} from 'semantic-ui-react';
+import type {DropdownProps} from 'semantic-ui-react';
+import type {TableCellProps} from './TableInput';
+
+export function DropdownCell<D extends Record<string, unknown>>(dropdownProps: DropdownProps) {
+	return function DropdownCellFn(props: TableCellProps<D>) {
+		const {
+			value,
+			row: {index: rowIndex},
+			column: {id},
+			updateData,
+		} = props;
+
+		// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+		const {value: v, onChange, ...rest} = dropdownProps;
+
+		return (
+			<Dropdown
+				{...rest}
+				value={value}
+				onChange={(event, val) => {
+					updateData(rowIndex, id, val.value);
+					if (onChange) onChange(event, val);
+				}}
+			/>
+		);
+	};
+}
