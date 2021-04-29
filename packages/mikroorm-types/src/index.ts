779399b4396547fdc3d3ac62bffff2e428d6e11f
Jonathan Spomer <38764827+JohnnySpomer@users.noreply.github.com>
2021-04-29 15:39:18 -0500

    feat(mikroorm-types): removed imageProperty

--- packages/mikroorm-types/src/index.ts
+++ packages/mikroorm-types/src/index.ts
@@ -2,4 +2,3 @@ export {LocalDateProperty} from './LocalDateProperty';
 export {JsonStringProperty} from './JsonStringProperty';
 export {MoneyProperty} from './MoneyProperty';
 export {DecimalProperty} from './DecimalProperty';
-export {ImageProperty} from './ImageProperty';
