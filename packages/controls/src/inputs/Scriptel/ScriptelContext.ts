8306a15d05a6b9ed4042c69afaac2e254a6ee4e1
darkadept <darkadept@durbn.net>
2021-05-11 12:06:40 -0500

    chore: updated

--- packages/controls/src/inputs/Scriptel/ScriptelContext.ts
+++ packages/controls/src/inputs/Scriptel/ScriptelContext.ts
@@ -2,6 +2,7 @@ import React, {MutableRefObject} from 'react';
 import type {ScriptelSocket} from './scriptel';
 import type {RenderedImage} from './scriptel/messages';
 
-export const ScriptelContext = React.createContext<
-	{socket: MutableRefObject<ScriptelSocket | undefined>; renderImage: RenderedImage | undefined; loading: boolean; isSigning: boolean} | undefined
->(undefined);
+export const ScriptelContext =
+	React.createContext<
+		{socket: MutableRefObject<ScriptelSocket | undefined>; renderImage: RenderedImage | undefined; loading: boolean; isSigning: boolean} | undefined
+	>(undefined);
--- packages/ordereddataloader/src/index.ts
+++ packages/ordereddataloader/src/index.ts
@@ -29,11 +29,11 @@ export default class OrderedDataLoader<K, V, C = K> {
 		const docsMap = new Map<C, V>();
 		unorderedResults.forEach(doc => {
 			const key: K = get(doc, this.options.idField || 'id');
-			const sKey = this.options.cacheKeyFn ? this.options.cacheKeyFn(key) : ((identity(key) as unknown) as C);
+			const sKey = this.options.cacheKeyFn ? this.options.cacheKeyFn(key) : (identity(key) as unknown as C);
 			docsMap.set(sKey, doc);
 		});
 		return keys.map(key => {
-			const sKey = this.options.cacheKeyFn ? this.options.cacheKeyFn(key) : ((identity(key) as unknown) as C);
+			const sKey = this.options.cacheKeyFn ? this.options.cacheKeyFn(key) : (identity(key) as unknown as C);
 			return docsMap.get(sKey);
 		});
 	}
