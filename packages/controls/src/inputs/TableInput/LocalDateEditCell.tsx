74f0b1e7809816c28bd9d77b68b0099786b6e619
Shayne Thiessen <shayne@shaynethiessen.com>
2022-06-20 14:59:13 -0500

    fix: removed opts

--- packages/controls/src/inputs/TableInput/LocalDateEditCell.tsx
+++ packages/controls/src/inputs/TableInput/LocalDateEditCell.tsx
@@ -2,17 +2,11 @@ import debug from 'debug';
 import {useState} from 'react';
 import {LocalDate} from '@js-joda/core';
 import type {TableCellProps} from './TableInput';
-import type {AddRowOnTabIf} from './addRowOnTab';
 import {LocalDatePicker} from '../../date/LocalDatePicker';
 
 const d = debug('thx.controls.inputs.TableInput.LocalDateEditCell');
 
-interface LocalDateEditCellOptions<D extends Record<string, unknown>> {
-	/** If function is present, and returns true, will add a new row if tab is pressed on the last row */
-	addRowOnTabIf?: AddRowOnTabIf<D, LocalDate>;
-}
-
-export function LocalDateEditCell<D extends Record<string, unknown>>(opts?: LocalDateEditCellOptions<D>) {
+export function LocalDateEditCell<D extends Record<string, unknown>>() {
 	return function LocalDateEditCellFn(props: TableCellProps<D, LocalDate>) {
 		const {
 			value: initialValue,
