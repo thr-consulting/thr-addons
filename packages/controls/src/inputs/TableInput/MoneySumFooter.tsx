0175c33d13b592469abf37354f91c4f5994a2204
Shayne Thiessen <shayne@shaynethiessen.com>
2022-07-26 13:55:59 -0500

    feat: check if type is money before attempting math

--- packages/controls/src/inputs/TableInput/MoneySumFooter.tsx
+++ packages/controls/src/inputs/TableInput/MoneySumFooter.tsx
@@ -1,15 +1,26 @@
 import {formatMoney, toMoney} from '@thx/money';
 import type {IdType, TableInstance} from 'react-table';
+import type Money from 'js-money';
 
 interface MoneySumFooterOptions<A extends Record<string, unknown>> {
 	id: IdType<A>;
 }
 
+type Row = {
+	values: Record<IdType<Money>, Money>;
+};
+
 export function MoneySumFooter<A extends Record<string, unknown>>(options: MoneySumFooterOptions<A>) {
 	const {id} = options || {};
 
 	return function MoneySumFooterInstance(info: TableInstance<A>) {
-		const sum = info.rows.reduce((memo, row) => memo.add(row.values[id]), toMoney());
+		const sum = info.rows.reduce((memo, row: Row) => {
+			// checks to make sure we have an instance of money -STT
+			if (row.values[id]?.currency && row.values[id]?.amount) {
+				return memo.add(row.values[id]);
+			}
+			return memo;
+		}, toMoney());
 		return <div style={{textAlign: 'right'}}>{formatMoney(sum)}</div>;
 	};
 }
