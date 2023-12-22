76f1419e7ff98881958694434e333247c597fac2
Spoowy <erwin.flaming@gmail.com>
2023-12-22 14:15:59 -0600

    fix(controls): lint

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -3,5 +3,14 @@ export {getMonthNames} from './getMonthNames';
 export {getNumberOfDaysInMonth} from './getNumberOfDaysInMonth';
 export {toLocalDate, toLocalDateTime, toDate, toEpochDay, toLocalTime} from './conversion';
 export {getTimeOfDay} from './specialFormats';
-export {getFiscalYear, getFiscalPeriod, getFiscalYearRange, getFiscalQuarterRange, getFiscalQuarter, getFiscalRange, PeriodEnumLookup, PeriodEnum} from './fiscal';
+export {
+	getFiscalYear,
+	getFiscalPeriod,
+	getFiscalYearRange,
+	getFiscalQuarterRange,
+	getFiscalQuarter,
+	getFiscalRange,
+	PeriodEnumLookup,
+	PeriodEnum,
+} from './fiscal';
 export type {FiscalDateRange} from './fiscal';
