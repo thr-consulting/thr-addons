4333c1f37b28d580652dff8a838cd406c96d5950
Shayne Thiessen <shayne@shaynethiessen.com>
2025-05-27 14:23:05 -0500

    feat: drop scriptel

--- packages/pdfform/src/index.ts
+++ packages/pdfform/src/index.ts
@@ -1,5 +1,3 @@
-export {signPdf, signPdfDoc, signPdfStream} from './signPdf';
-export type {PDFSignature} from './signPdf';
 export {fillPdfForm, fillPdfFormStream, fillPdfFormDoc} from './fillPdf';
 export type {PdfFormDataType} from './fillPdf';
 export type {PdfInputType} from './types';
